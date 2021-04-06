90bb93a9025a45751954513ea810d4f6fea3d3ad
Edu <eduardez96@gmail.com>
2021-04-06 23:19:25 +0200

    a ver si con esto va

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');  
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,7 +9,6 @@ var indexRouter = require('./routes/index');
 var postsRouter = require('./routes/posts');
 
 var bodyParser  = require("body-parser");  
-var cors = require('cors');  
 
 require('dotenv').config();
 
